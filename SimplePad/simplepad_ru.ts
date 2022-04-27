da6eae41fa2acb41eaae2677274a9a23ca93f876
Alexandr <kajlaleksandr1986@gmail.com>
2022-04-27 21:26:22 +0700

    commit

--- SimplePad/simplepad_ru.ts
+++ SimplePad/simplepad_ru.ts
@@ -4,25 +4,30 @@
 <context>
     <name>SimplePad</name>
     <message>
-        <location filename="SimplePad.cpp" line="27"/>
-        <location filename="SimplePad.cpp" line="46"/>
+        <location filename="SimplePad.cpp" line="38"/>
+        <location filename="SimplePad.cpp" line="57"/>
         <source>Text file(*.txt);; All(*.*)</source>
         <translation>Текстовый файл(*.txt);; Все(*.*)</translation>
     </message>
     <message>
-        <location filename="SimplePad.cpp" line="26"/>
+        <location filename="SimplePad.cpp" line="37"/>
         <source>Save file</source>
         <translation>Сохранить файл</translation>
     </message>
     <message>
-        <location filename="SimplePad.cpp" line="45"/>
+        <location filename="SimplePad.cpp" line="56"/>
         <source>Open file</source>
         <translation>Открыть файл</translation>
     </message>
     <message>
-        <location filename="SimplePad.cpp" line="89"/>
+        <location filename="SimplePad.cpp" line="100"/>
         <source>Info</source>
         <translation>Справка</translation>
+    </message>
+    <message>
+        <location filename="SimplePad.cpp" line="160"/>
+        <source>Select folder</source>
+        <translation>Выбрать папку</translation>
     </message>
 </context>
 <context>
@@ -33,72 +38,77 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="SimplePad.ui" line="34"/>
+        <location filename="SimplePad.ui" line="60"/>
         <source>&amp;File</source>
         <translation>&amp;Файл</translation>
     </message>
     <message>
-        <location filename="SimplePad.ui" line="42"/>
+        <location filename="SimplePad.ui" line="69"/>
         <source>&amp;Interface</source>
         <translation>&amp;Интерфейс</translation>
     </message>
     <message>
-        <location filename="SimplePad.ui" line="46"/>
+        <location filename="SimplePad.ui" line="73"/>
         <source>&amp;Language</source>
         <translation>&amp;Язык</translation>
     </message>
     <message>
-        <location filename="SimplePad.ui" line="53"/>
+        <location filename="SimplePad.ui" line="80"/>
         <source>&amp;Color theme</source>
         <oldsource>&amp;Theme</oldsource>
         <translation>&amp;Цветовая тема</translation>
     </message>
     <message>
-        <location filename="SimplePad.ui" line="63"/>
+        <location filename="SimplePad.ui" line="90"/>
         <source>&amp;Info</source>
         <translation>&amp;Справка</translation>
     </message>
     <message>
-        <location filename="SimplePad.ui" line="86"/>
+        <location filename="SimplePad.ui" line="113"/>
         <source>&amp;Open file</source>
         <oldsource>&amp;Open File</oldsource>
         <translation>&amp;Открыть файл</translation>
     </message>
     <message>
-        <location filename="SimplePad.ui" line="95"/>
+        <location filename="SimplePad.ui" line="122"/>
         <source>O&amp;pen file(Read only)</source>
         <oldsource>O&amp;pen File(Read only)</oldsource>
         <translation>О&amp;ткрыть файл(Только для читение)</translation>
     </message>
     <message>
-        <location filename="SimplePad.ui" line="105"/>
+        <location filename="SimplePad.ui" line="132"/>
         <source>&amp;Save</source>
         <translation>&amp;Сохранить</translation>
     </message>
     <message>
-        <location filename="SimplePad.ui" line="110"/>
+        <location filename="SimplePad.ui" line="137"/>
         <source>&amp;English</source>
         <translation>&amp;Английский</translation>
     </message>
     <message>
-        <location filename="SimplePad.ui" line="115"/>
+        <location filename="SimplePad.ui" line="142"/>
         <source>&amp;Russian</source>
         <translation>&amp;Русский</translation>
     </message>
     <message>
-        <location filename="SimplePad.ui" line="124"/>
+        <location filename="SimplePad.ui" line="151"/>
         <source>&amp;About SimplePad</source>
         <translation>&amp;О SimplePad</translation>
     </message>
     <message>
-        <location filename="SimplePad.ui" line="129"/>
+        <location filename="SimplePad.ui" line="156"/>
         <source>&amp;Light</source>
         <translation>&amp;Светлая</translation>
     </message>
     <message>
-        <location filename="SimplePad.ui" line="134"/>
+        <location filename="SimplePad.ui" line="161"/>
         <source>&amp;Dark</source>
         <translation>&amp;Тёмная</translation>
+    </message>
+    <message>
+        <location filename="SimplePad.ui" line="166"/>
+        <source>Se&amp;lect folder as project </source>
+        <translation>Открыть &amp;папку как проект </translation>
     </message>
 </context>
 </TS>
