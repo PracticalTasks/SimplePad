3977eb90345441a5d73b5fed2b0f73a9a6e83d8c
Alexandr <Alexandr@ASUSTUF>
2022-08-10 13:10:33 +0700

    commit

--- SimplePad/Test.cpp
+++ SimplePad/Test.cpp
@@ -1,6 +1,6 @@
 #include<QtTest/QtTest>
 #include<QtWidgets>
-#include"FileSys.h"
+#include"HotKeys.h"
 
 class TestFileSys : public QObject
 {
@@ -13,14 +13,6 @@ private slots:
 //Полу автамотический тест
 void TestFileSys::testOpenFile()
 {
-	FileSys fs;
-	//Нажать Отмена
-	fs.openFile();
-	QCOMPARE(fs.getAddrFile()->fileName(), "");
-	//Нажать открыть
-	fs.openFile("Test.txt");
-	QString str = QFINDTESTDATA("Test.txt");
-	QCOMPARE(fs.getAddrFile()->fileName(), str);
 
 }
 
